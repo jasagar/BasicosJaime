308873bd2841bd7b7ec7c7d0ecc3aa8e4bde8d4a
Jaime Sansano García <jasano2@hotmail.com>
2019-12-09 09:32:36 +0100

    creación del método cadenas()

--- src/basicosjaime/BasicosJaime.java
+++ src/basicosjaime/BasicosJaime.java
@@ -3,7 +3,22 @@ package basicosjaime;
 public class BasicosJaime {
 
 	public static void main(String[] args) {
+		
+		cadenas();
 
 	}
+	
+	public static void cadenas() {
+
+        // declaración
+        String texto;
+
+        // asignación
+        texto = "Esta cadena de texto se ha creado con la clase String";
+
+        // salida
+        System.out.println("*** Ejemplo de cadena de texto:");
+        System.out.println(texto);
+    }
 
 }
